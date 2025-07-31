a5250560c39cba772e5c3c589dc9cd8f1ac7a481
gbtrajano <gbtrajano1@gmail.com>
2025-07-31 15:45:29 -0300

    fix: absolute position

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -4,7 +4,7 @@ import { FaCalendar, FaUmbrellaBeach, FaMoneyBillAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="absolute left-1/2 -translate-x-1/2 -translate-y-24 border border-gray-200/80 py-3 px-2 flex gap-2 shadow-lg rounded-xl bg-white/80 hover:shadow-xl transition-all duration-300">
+    <div className="absolute left-1/2 -translate-x-1/2 -translate-y-32 border border-gray-200/80 py-3 px-2 flex gap-2 shadow-lg rounded-xl bg-white/80 hover:shadow-xl transition-all duration-300">
       <div className="group relative px-3 cursor-pointer">
         <div className="flex h-10 w-10 items-center justify-center rounded-lg transition-colors duration-300">
           <Link href="/">
