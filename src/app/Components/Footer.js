bf215fb25c97206548d0db6c33d081c617069418
gbtrajano <gbtrajano1@gmail.com>
2025-07-31 15:42:34 -0300

    fix: footer position

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -4,7 +4,7 @@ import { FaCalendar, FaUmbrellaBeach, FaMoneyBillAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-200/80 py-3 px-2 flex gap-2 shadow-lg rounded-xl bg-white/80 backdrop-blur-sm hover:shadow-xl transition-all duration-300">
+    <div className="absolute left-1/2 -translate-x-1/2 -translate-y-24 border border-gray-200/80 py-3 px-2 flex gap-2 shadow-lg rounded-xl bg-white/80 hover:shadow-xl transition-all duration-300">
       <div className="group relative px-3 cursor-pointer">
         <div className="flex h-10 w-10 items-center justify-center rounded-lg transition-colors duration-300">
           <Link href="/">
