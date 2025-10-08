94f0bc83d605f1c8721a2536fb1d24be1741c9f6
you@example.com <gbtrajano@users.noreply.github.com>
2025-10-08 10:10:48 -0300

    teste

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
